43133d3677f8321dd21f81d90fab3b57a9f22513
Dai Ngo Tan <ngotandai95@gmail.com>
2021-09-26 15:50:36 -0400

    update

--- 3_NoteShare/FileTransfer/server.py
+++ 3_NoteShare/FileTransfer/server.py
@@ -23,9 +23,10 @@ class ClientThread(threading.Thread):
         self.__client_socket = client_socket
         self.__network_buffer_size = 4096
         self.__db = Database(name="NoteShare")
+        self.__run()
     
 
-    def send_email(self, email=None, username=None, password=None):
+    def __send_email(self, email=None, username=None, password=None):
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.ehlo()
             smtp.starttls()
@@ -41,47 +42,46 @@ class ClientThread(threading.Thread):
             smtp.sendmail(EMAIL_ADDRESS, email, message)
 
 
-    def run(self):
-        while True:
-            client_message_raw = self.__client_socket.recv(self.__network_buffer_size)
-            client_message = ClientMessage.process_message(client_message_raw)
+    def __run(self):
+        client_message_raw = self.__client_socket.recv(self.__network_buffer_size)
+        client_message = ClientMessage.process_message(client_message_raw)
 
-            if client_message["password"]:
-                hashed_password = hashlib.sha256(client_message["password"].encode()).hexdigest()
+        if client_message["password"]:
+            hashed_password = hashlib.sha256(client_message["password"].encode()).hexdigest()
 
-            if client_message["action"] == "login":
-                success_result, message_result = self.__db.login(username=client_message["username"], 
-                                                                 password=hashed_password)
-            elif client_message["action"] == "register":
-                success_result, message_result = self.__db.register(username=client_message["username"],
-                                                                    password=hashed_password,
-                                                                    email=client_message["email"])
-                if success_result:
-                    self.send_email()
-            elif client_message["action"] == "password email":
-                success_result, message_result, username = self.__db.update_password_email(email=client_message["email"],
-                                                                                           new_password=hashed_password)
-                if success_result:
-                    self.send_email(email=client_message["email"], username=username, password=hashed_password)
-            elif client_message["action"] == "password username":
-                success_result, message_result = self.__db.update_password_username(username=client_message["username"],
-                                                                                    new_password=hashed_password)
-            elif client_message["action"] == "get all files":
-                success_result, message_result = self.__db.get_all_notes()
-            elif client_message["action"] == "upload":
-                success_result, message_result = self.__db.add_note(filename=client_message["filename"],
-                                                                    username=client_message["username"],
-                                                                    tag=client_message["tag"],
-                                                                    created=datetime.now(pytz.timezone("America/Chicago")))
-            elif client_message["action"] == "download":
-                ClientMessage.receiving_file(filename=client_message["filename"], client_socket=self.__client_socket,
-                                             network_buffer_size=self.__network_buffer_size)
-                ServerMessage.sending_file(filename=client_message["filename"], client_socket=self.__client_socket,
-                                           network_buffer_size=self.__network_buffer_size)
-                success_result, message_result = True, "Your note has been donwloaded!"
+        if client_message["action"] == "login":
+            success_result, message_result = self.__db.login(username=client_message["username"], 
+                                                                password=hashed_password)
+        elif client_message["action"] == "register":
+            success_result, message_result = self.__db.register(username=client_message["username"],
+                                                                password=hashed_password,
+                                                                email=client_message["email"])
+            if success_result:
+                self.__send_email()
+        elif client_message["action"] == "password email":
+            success_result, message_result, username = self.__db.update_password_email(email=client_message["email"],
+                                                                                        new_password=hashed_password)
+            if success_result:
+                self.__send_email(email=client_message["email"], username=username, password=hashed_password)
+        elif client_message["action"] == "password username":
+            success_result, message_result = self.__db.update_password_username(username=client_message["username"],
+                                                                                new_password=hashed_password)
+        elif client_message["action"] == "get all files":
+            success_result, message_result = self.__db.get_all_notes()
+        elif client_message["action"] == "upload":
+            success_result, message_result = self.__db.add_note(filename=client_message["filename"],
+                                                                username=client_message["username"],
+                                                                tag=client_message["tag"],
+                                                                created=datetime.now(pytz.timezone("America/Chicago")))
+        elif client_message["action"] == "download":
+            ClientMessage.receiving_file(filename=client_message["filename"], client_socket=self.__client_socket,
+                                            network_buffer_size=self.__network_buffer_size)
+            ServerMessage.sending_file(filename=client_message["filename"], client_socket=self.__client_socket,
+                                        network_buffer_size=self.__network_buffer_size)
+            success_result, message_result = True, "Your note has been donwloaded!"
 
-            self.__client_socket.sendall(ServerMessage(success=success_result, message=message_result).to_json())
-            self.__client_socket.close()
+        self.__client_socket.sendall(ServerMessage(success=success_result, message=message_result).to_json())
+        self.__client_socket.close()
             
 
 class Server:
