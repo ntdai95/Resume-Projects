0a508631e8ead7420479efff7d1f4e59741bcedb
Dai Ngo Tan <ngotandai95@gmail.com>
2021-11-21 19:42:45 -0500

    update

--- (Potential) Distributed Password Cracking System/password_hashing.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import argparse
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog = "password hashing", description = "Password Hashing")
-    parser.add_argument("password", help = "Please, type in the password that you want to hash.")
-    args = parser.parse_args()
-
-    print(hashlib.md5(args.password.encode()).hexdigest())
