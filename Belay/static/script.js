a9ab8f77c69fbe0ebddc00c1fd8543f7b69e8f43
Dai Ngo Tan <ngotandai95@gmail.com>
2022-04-27 16:02:20 -0400

    update

--- Belay/static/script.js
+++ Belay/static/script.js
@@ -1,5 +1,6 @@
 /* For auth */
 
+
 function signup() {
     let username = document.getElementById("usernamesignup").value;
     let password = document.getElementById("passwordsignup").value;
@@ -14,12 +15,13 @@ function signup() {
     .then(data => {
         if (data.success == true) {
             localStorage.setItem('ntdai95_belay_authkey', data.authkey);
+            localStorage.setItem('ntdai95_belay_username', username);
 
             let pushStateState = {"authkey": data.authkey, "username": username};
             let pushStateUnused = null;
             let pushStateUrl = null;
             if (magiclink !== null && magiclink !== "null") {
-                pushStateState = {...pushStateState, ...history.state}
+                pushStateState = {...pushStateState, ...history.state};
                 pushStateUnused = "User Entered Page";
                 pushStateUrl = magiclink;
                 localStorage.setItem('ntdai95_belay_magiclink', null);
@@ -58,12 +60,13 @@ function login() {
     .then(data => {
         if (data.success == true) {
             localStorage.setItem('ntdai95_belay_authkey', data.authkey);
+            localStorage.setItem('ntdai95_belay_username', username);
 
             let pushStateState = {"authkey": data.authkey, "username": username};
             let pushStateUnused = null;
             let pushStateUrl = null;
             if (magiclink !== null && magiclink !== "null") {
-                pushStateState = {...pushStateState, ...history.state}
+                pushStateState = {...pushStateState, ...history.state};
                 pushStateUnused = "User Entered Page";
                 pushStateUrl = magiclink;
                 localStorage.setItem('ntdai95_belay_magiclink', null);
@@ -131,7 +134,7 @@ function startChannelPolling() {
     .then(response => response.json())
     .then(data => {
         if (data.success == true) {
-            document.querySelector(".channelColumn h1").innerHTML = "Welcome, " + history.state['username'] + "!";
+            document.querySelector(".channelColumn h1").innerHTML = "Welcome, " + localStorage.getItem('ntdai95_belay_username') + "!";
             let channelTable = document.getElementsByClassName("channelTable");
             let channels = data.channels;
 
@@ -144,7 +147,7 @@ function startChannelPolling() {
                         button.setAttribute("type", "button");
                         button.setAttribute("id", "channel" + channels[i][0]);
                         button.setAttribute("value", "Go");
-                        button.setAttribute("onclick", "goToChannel('" + channels[i][0] + "', '" + decodeURIComponent(channels[i][1]) + "');");
+                        button.setAttribute("onclick", "goToChannel('" + channels[i][0] + "');");
 
                         button.innerHTML = decodeURIComponent(channels[i][1]);
                         td.appendChild(button);
@@ -168,9 +171,10 @@ function startChannelPolling() {
 }
 
 
-function goToChannel(channel_id, channelName) {
+function goToChannel(channel_id) {
     let authkey = localStorage.getItem("ntdai95_belay_authkey");
-    let pushStateState = {"authkey": authkey, "username": history.state['username'], "channel_id": channel_id, "channelName": channelName};
+    let username = localStorage.getItem("ntdai95_belay_username");
+    let pushStateState = {"authkey": authkey, "username": username, "channel_id": channel_id};
     let pushStateUnused = "Channel Page";
     let pushStateUrl = "http://127.0.0.1:5000/channel/" + channel_id;
 
@@ -181,8 +185,8 @@ function goToChannel(channel_id, channelName) {
 
 function postMessage() {
     let newMessage = encodeURIComponent(document.getElementById("newMessage").value);
-    let username = history.state['username'];
     let authkey = localStorage.getItem("ntdai95_belay_authkey");
+    let username = localStorage.getItem('ntdai95_belay_username');
 
     fetch("http://127.0.0.1:5000/api/channel/" + history.state["channel_id"] + "/message", {
         method: 'POST',
@@ -209,7 +213,7 @@ function getMessages() {
     let promise = fetch("http://127.0.0.1:5000/api/channel/" + history.state["channel_id"] + "/message", {
         method: 'GET',
         headers: {'Authorization': authkey, 'Content-Type': 'application/json'}
-    })
+    });
     
     return promise;
 }
@@ -226,8 +230,8 @@ function startMessagePolling() {
             if (document.querySelector("#channelID" + history.state["channel_id"]) == null) {
                 messageContent.innerHTML = "";
             }
-            let messages = data.messages;
 
+            let messages = data.messages;
             if (messages.length > 0) {
                 for (let i = 0; i < messages.length; i++) {
                     if (document.querySelector("#message" + messages[i][0]) == null) {
@@ -275,7 +279,8 @@ function startMessagePolling() {
 
 function goToThread(message_id) {
     let authkey = localStorage.getItem("ntdai95_belay_authkey");
-    let pushStateState = {"authkey": authkey, "username": history.state['username'], "channel_id": history.state['channel_id'], "channelName": history.state['channelName'], "message_id": message_id};
+    let username = localStorage.getItem("ntdai95_belay_username");
+    let pushStateState = {"authkey": authkey, "username": username, "channel_id": history.state['channel_id'], "message_id": message_id};
     let pushStateUnused = "Thread Page";
     let pushStateUrl = "http://127.0.0.1:5000/channel/" + history.state['channel_id'] + "/thread/" + message_id;
 
@@ -286,7 +291,7 @@ function goToThread(message_id) {
 
 function replyMessage() {
     let newReply = encodeURIComponent(document.getElementById("newReply").value);
-    let username = history.state['username'];
+    let username = localStorage.getItem('ntdai95_belay_username');
     let authkey = localStorage.getItem("ntdai95_belay_authkey");
 
     fetch("http://127.0.0.1:5000/api/channel/" + history.state["channel_id"] + "/message/" + history.state["message_id"], {
@@ -343,7 +348,7 @@ function startReplyPolling() {
                 button.setAttribute("class", "replyThread");
                 button.setAttribute("type", "button");
                 button.setAttribute("value", "Go");
-                button.setAttribute("onclick", "goToChannel('" + data.message[3] + "', '" + history.state['channelName'] + "');");
+                button.setAttribute("onclick", "goToChannel('" + data.message[3] + "');");
                 button.innerHTML = "Go To Channel";
                 let h3Replies = document.createElement('h3');
                 h3Replies.innerHTML = "Replies:";
@@ -402,26 +407,32 @@ function startReplyPolling() {
 
 /* For all */
 
+
 function checkAuthkey() {
-    let authkey = localStorage.getItem('authkey');
+    let authkey = localStorage.getItem('ntdai95_belay_authkey');
+    let username = localStorage.getItem('ntdai95_belay_username');
+    let paths = window.location.pathname.split('/');
+    let pushStateState = {"authkey": authkey, "username": username};
+    if (paths.length == 3) {
+        pushStateState["channel_id"] = paths[2];
+    } 
+    else if (paths.length == 5) {
+        pushStateState["channel_id"] = paths[2];
+        pushStateState["message_id"] = paths[4];
+    }
+
+    let pushStateUnused = "";
+    let pushStateUrl = "";
     if (authkey === null || authkey === "null") {
         localStorage.setItem("ntdai95_belay_magiclink", document.URL);
-        let paths = window.location.pathname.split('/');
-        let pushStateState = {};
-        if (paths.length == 3) {
-            pushStateState = {"channel_id": paths[2], "channelName": ""}
-        } 
-        else if (paths.length == 5) {
-            pushStateState = {"channel_id": paths[2], "channelName": "", "message_id": paths[4]}
-        }
-
-        let pushStateUnused = "Login/Signup Page";
-        let pushStateUrl = "http://127.0.0.1:5000";
-
-        checkState(true, pushStateState, pushStateUnused, pushStateUrl);
+        pushStateUnused = "Login/Signup Page";
+        pushStateUrl = "http://127.0.0.1:5000";
     } else {
-        checkState();
+        pushStateUnused = "User Copied Link Page";
+        pushStateUrl = document.URL;
     }
+
+    checkState(true, pushStateState, pushStateUnused, pushStateUrl);
     return;
 }
 
